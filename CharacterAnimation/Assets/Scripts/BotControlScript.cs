9a772abfd66da6af0113c4d0209cb80579679fbe
whileLooper <bochentheone@gmail.com>
2015-06-04 16:32:59 -0400

    adding movement functions

---
+++ CharacterAnimation/Assets/Scripts/BotControlScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BotControlScript : MonoBehaviour {
+
+	private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate(){
+		float h = Input.GetAxis ("Horizontal");
+		float v = Input.GetAxis ("Vertical");
+		anim.SetFloat ("Speed", v);
+		anim.SetFloat ("Direction", h);
+	}
+}

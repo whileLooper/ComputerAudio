1f7964ebe9c3b0a6994eff7bc30557b74b45eda5
whileLooper <bochentheone@gmail.com>
2015-06-05 22:03:24 -0400

    Added Camera to Model

--- CharacterAnimation/Assets/Scripts/ThirdPersonCamera.cs
+++ CharacterAnimation/Assets/Scripts/ThirdPersonCamera.cs
@@ -2,14 +2,18 @@
 using System.Collections;
 
 public class ThirdPersonCamera : MonoBehaviour {
+	public float smooth = 3f;		// a public variable to adjust smoothing of camera motion
+	Transform standardPos;			// the usual position for the camera, specified by a transform in the game
 
 	// Use this for initialization
 	void Start () {
-	
+		standardPos = GameObject.Find ("CamPos").transform;
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		transform.position = Vector3.Lerp(transform.position, standardPos.position, Time.deltaTime * smooth);	
+		transform.forward = Vector3.Lerp(transform.forward, standardPos.forward, Time.deltaTime * smooth);
 	}
 }

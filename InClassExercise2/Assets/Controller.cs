c3cae8ae32d487a04ffadec57a62d8e8c7334620
whileLooper <bochentheone@gmail.com>
2015-06-16 22:39:13 -0400

    sonification assignment complete

---
+++ InClassExercise2/Assets/Controller.cs
@@ -0,0 +1,175 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Controller : MonoBehaviour {
+	
+	public AudioSource[] sources;
+	public Slider HeartRateSlider, RespirationRateSlider, HeartVolumeSlider, RespirationVolumeSlider, GSRSlider, TruthfulSlider, LyingSlider;
+	AudioSource heartBeat, respirationIn, respirationOut, gsrAlert, truthAlert, lieAlert;
+	
+	float currentTimeInSeconds;
+	
+	float lastHeartPlayTime;
+	float lastRespirationPlayTime;
+
+	float heartRate = 1.5f;
+	float respirationRate = 3f;
+
+	bool first = true, gsrOneAlert = false, gsrCount = false, truthCount = false, lieCount = false, truthOneAlert = false, lieOneAlert = false;
+	
+	// Use this for initialization
+	void Start () {
+		//start my sound playback based on initial values
+		
+		currentTimeInSeconds = 0;
+		lastHeartPlayTime = 0;
+		lastRespirationPlayTime = 0;
+		heartBeat = sources [0];
+		respirationIn = sources [1];
+		respirationOut = sources [2];
+		gsrAlert = sources [3];
+		truthAlert = sources [4];
+		lieAlert = sources [5];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		currentTimeInSeconds = currentTimeInSeconds + Time.deltaTime;
+		//Debug.Log ("Current time: " + currentTimeInSeconds);
+		float heartTimeSinceLastSoundPlayback = currentTimeInSeconds - lastHeartPlayTime;
+		float respirationTimeSinceLastSoundPlayback = currentTimeInSeconds - lastRespirationPlayTime;
+		if (heartTimeSinceLastSoundPlayback > heartRate) {
+			lastHeartPlayTime = currentTimeInSeconds;
+			heartBeat.Play ();
+		}
+		if (respirationTimeSinceLastSoundPlayback > respirationRate/2) {
+			lastRespirationPlayTime = currentTimeInSeconds;
+			if (first) {
+				respirationIn.Play();
+			} else {
+				respirationOut.Play();
+			}
+			first = !first;
+		}
+
+		// GSR
+		if (Mathf.Abs (RespirationRateSlider.value - HeartRateSlider.value*3) >= 0.5f && !gsrCount) {
+			gsrOneAlert = true;
+			gsrCount = true;
+		}
+		if (gsrOneAlert) {
+			gsrAlert.Play ();
+			gsrOneAlert = false;
+		}
+
+		if (Mathf.Abs (RespirationRateSlider.value - HeartRateSlider.value * 3) < 0.5f) {
+			gsrCount = false;
+		}
+
+		// Truth
+		if (Mathf.Abs (RespirationRateSlider.value - HeartRateSlider.value * 3) <= 0.1f && !truthCount && RespirationRateSlider.value >= 2f) {
+			truthOneAlert = true;
+			truthCount = true;
+		}
+		if (truthOneAlert) {
+			truthAlert.Play ();
+			truthOneAlert = false;
+		}
+		if (Mathf.Abs (RespirationRateSlider.value - HeartRateSlider.value * 3) > 0.1f || RespirationRateSlider.value < 2f) {
+			truthCount = false;
+		}
+
+		if (Mathf.Abs (RespirationRateSlider.value - HeartRateSlider.value * 3) >= 1.3f && !lieCount && HeartRateSlider.value < 0.4f && RespirationRateSlider.value > 2.5f) {
+			lieOneAlert = true;
+			lieCount = true;
+		}
+		if (lieOneAlert) {
+			lieAlert.Play ();
+			lieOneAlert = false;
+		}
+		if (Mathf.Abs (RespirationRateSlider.value - HeartRateSlider.value * 3) < 1.3f && !lieCount || HeartRateSlider.value > 0.4f || RespirationRateSlider.value < 2.5f) {
+			lieCount = false;
+		}
+	}
+	
+	public void OnHeartRateChange()
+	{
+		heartRate = HeartRateSlider.value;
+		Debug.Log (HeartRateSlider.value);
+	}
+	
+	public void OnRespirationRateChange()
+	{
+		respirationRate = RespirationRateSlider.value;
+	}
+
+	public void OnHeartVolumeChange() {
+		heartBeat.volume = HeartVolumeSlider.value;
+	}
+
+	public void OnRespirationVolumeChange() {
+		respirationIn.volume = RespirationVolumeSlider.value;
+		respirationOut.volume = RespirationVolumeSlider.value;
+	}
+
+	public void OnGSRChange() {
+		gsrAlert.volume = GSRSlider.value;
+	}
+
+	public void ActivateGSR() {
+		HeartRateSlider.value = Random.Range (0.2f, 0.48f);
+		float random = Random.Range (0.0f, 1.0f);
+		OnHeartRateChange ();
+		if (random <= 0.5f) {
+			RespirationRateSlider.value = Random.Range (HeartRateSlider.value * 3 + 0.500001f, 3f);
+		} else {
+			RespirationRateSlider.value = Random.Range (0.11f, HeartRateSlider.value * 3 - 0.500001f);
+		}
+		OnRespirationRateChange ();
+		GSRSlider.value = (Mathf.Abs (RespirationRateSlider.value - HeartRateSlider.value) / 2.7f);
+		Debug.Log ("ActivateGSR " + RespirationRateSlider.value);
+		Debug.Log ("ActivateGSR " + HeartRateSlider.value);
+		Debug.Log ("ActivateGSR " + Mathf.Abs (RespirationRateSlider.value - HeartRateSlider.value) / 2.7f);
+	}
+
+	public void OnTruthfulChange() {
+		truthAlert.volume = TruthfulSlider.value;
+	}
+
+	public void ActivateCalm() {
+		HeartRateSlider.value = Random.Range (0.9f, 1f);
+		OnHeartRateChange ();
+		RespirationRateSlider.value = Random.Range (2.6f, 3f);
+		OnRespirationRateChange ();
+	}
+
+	public void OnExcitedTrue() {
+		HeartRateSlider.value = Random.Range (0.6f, 0.9f);
+		OnHeartRateChange ();
+		float random = Random.Range (0.0f, 1.0f);
+		if (random <= 0.5f) {
+			RespirationRateSlider.value = Random.Range (HeartRateSlider.value * 3, HeartRateSlider.value * 3 + 0.09999f);
+		} else {
+			RespirationRateSlider.value = Random.Range (HeartRateSlider.value * 3 - 0.09999f, HeartRateSlider.value * 3);
+		}
+		OnRespirationRateChange ();
+		TruthfulSlider.value = 1f - Mathf.Abs (RespirationRateSlider.value - HeartRateSlider.value * 3) * 10;
+
+	}
+
+	public void OnExcitedLie() {
+		HeartRateSlider.value = Random.Range (0.1f, 0.4f);
+		OnHeartRateChange ();
+		RespirationRateSlider.value = Random.Range (2.5f, 3.0f);
+		OnRespirationRateChange ();
+		LyingSlider.value = Mathf.Abs (RespirationRateSlider.value - HeartRateSlider.value * 3) / 1.4f - (1.3f / 1.4f);
+		Debug.Log ("Respiration: " + RespirationRateSlider.value);
+		Debug.Log ("Heart Rate: " + HeartRateSlider.value * 3);
+		Debug.Log (LyingSlider.value);
+	}
+
+	public void OnLieChange() {
+		lieAlert.volume = LyingSlider.value;
+	}
+}
--- PolygraphSonification/Assets/Scripts/EventControl.cs
+++ PolygraphSonification/Assets/Scripts/EventControl.cs
@@ -1,62 +1,158 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class EventControl : MonoBehaviour {
 
-	public AudioClip clip1;
-	public AudioClip clip2;
 
-	AudioSource sound1;
-	AudioSource sound2;
-	
-	float radiationPlaybackRate = 0;
-	float proximityPlaybackRate = 0;
-	
-	float currentSlider;
-	float lastSlider;
-	
-	float currentTimeInSeconds;
-	
-	float lastSoundPlaybackTime;
-	float playbackInterval = 1.5f;
-	
+	public Slider heartRateSlider, respPitchSlider, heartVolumeSlider, respVolumeSlider, GSRSlider, truthSlider, lieSlider;
+	public AudioSource[] clip;
+	AudioSource heartBeat, respiration, gsrAlert, truthAlert, lieAlert;
+
+	float currentTime, lastHeartTime, lastRespTime;
+
+	float heartRate = 1.5f;
+	float respRate = 1f;
+
+	bool isGsr = false;
+	bool gsrCount = false;
+	bool truthCount = false;
+	bool lieCount = false;
+	bool isTruth = false;
+	bool isLie = false;
+
 	// Use this for initialization
 	void Start () {
 		//start my sound playback based on initial values
+	
+		currentTime = 0;
+		lastHeartTime = 0;
+		lastRespTime = 0;
 
-		sound1 = gameObject.GetComponent<AudioSource> ();
-		sound2 = gameObject.GetComponent<AudioSource> ();
-
-		currentTimeInSeconds = 0;
-		lastSoundPlaybackTime = 0;
-		sound1.clip = clip1;
-		sound2.clip = clip2;
+		heartBeat = clip [0];
+		respiration = clip [1];
+		gsrAlert = clip [2];
+		truthAlert = clip [3];
+		lieAlert = clip [4];
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		currentTimeInSeconds = currentTimeInSeconds + Time.deltaTime;
-		//Debug.Log ("Current time: " + currentTimeInSeconds);
-		float timeSinceLastSoundPlayback = currentTimeInSeconds - lastSoundPlaybackTime;
-		if (timeSinceLastSoundPlayback > playbackInterval) {
-			Debug.Log ("Play back interval: " + playbackInterval);
-			Debug.Log ("Played sound at " + timeSinceLastSoundPlayback + " second(s).");
-			lastSoundPlaybackTime = currentTimeInSeconds;
-			sound1.Play ();
+
+		currentTime = currentTime + Time.deltaTime;
+		float timeSinceLastSoundPlayback = currentTime - lastHeartTime;
+		float timeSinceLastSoundPlayback2 = currentTime - lastRespTime;
+
+		if (timeSinceLastSoundPlayback > heartRate)
+		{
+			//Debug.Log ("Played sound at " + timeSinceLastSoundPlayback + " second(s).");
+			lastHeartTime = currentTime;
+			heartBeat.Play();
+		}
+		if (timeSinceLastSoundPlayback2 > respRate) {
+			Debug.Log ("Played sound at " + timeSinceLastSoundPlayback2 + " second(s).");
+
+			lastRespTime = currentTime;
+			respiration.Play();
+		}
+
+		if (Mathf.Abs (respPitchSlider.value - heartRateSlider.value*2.5f) > 1f 
+		    	&& !gsrCount) {
+			isGsr = true;
+			gsrCount = true;
+		}		
+		if (Mathf.Abs (respPitchSlider.value - heartRateSlider.value * 2.5f) < 0.5f) {
+			gsrCount = false;
+		}		
+		if (Mathf.Abs (respPitchSlider.value - heartRateSlider.value * 2.5f) <= 0.1f 
+		    && !truthCount && respPitchSlider.value >= 1.3f) {
+			isTruth = true;
+			truthCount = true;
+		}
+
+		if (Mathf.Abs (respPitchSlider.value - heartRateSlider.value * 2.5f) > 0.1f 
+		    || respPitchSlider.value < 2f) {
+			truthCount = false;
+		}
+		
+		if (Mathf.Abs (respPitchSlider.value - heartRateSlider.value * 2.5f) >= 1.3f 
+		    && !lieCount && heartRateSlider.value < 0.4f 
+		    && respPitchSlider.value > 2.5f) {
+			isLie = true;
+			lieCount = true;
+		}
+		if (Mathf.Abs (respPitchSlider.value - heartRateSlider.value * 2.5f) < 1.2f 
+		    && !lieCount || heartRateSlider.value > 0.5f 
+		    || respPitchSlider.value < 2.5f) {
+			lieCount = false;
+		}
+		if (isGsr) {
+			gsrAlert.Play ();
+			isGsr = false;
+		}
+		if (isTruth) {
+			truthAlert.Play ();
+			isTruth = false;
+		}
+		if (isLie) {
+			lieAlert.Play ();
+			isLie = false;
 		}
 	}
-	
-	public void OnRadiationCountChange(float sliderValue)
-	{
-		radiationPlaybackRate = sliderValue;
-		Debug.Log ("Radiation: " + radiationPlaybackRate);
-		playbackInterval = sliderValue;
+
+	public void heartBeatVolume(float sliderValue){
+		heartBeat.volume = sliderValue;
 	}
-	
-	public void OnProximityValueChange(float sliderValue)
-	{
-		proximityPlaybackRate = sliderValue;
-		Debug.Log ("Proximity: " + proximityPlaybackRate);
-		sound1.pitch = sliderValue;
+	public void heartRatePitch(float sliderValue){
+		heartBeat.pitch = sliderValue;
+	}
+	public void respVolume(float sliderValue){
+		respiration.volume = sliderValue;
+	}
+	public void respPicth(float sliderValue){
+		respiration.pitch = sliderValue;
+	}
+	public void gsrAlertVolume(float sliderValue){
+		gsrAlert.volume = sliderValue;
+	}
+	public void truthAlertVolume(float sliderValue){
+		truthAlert.volume = sliderValue;
+	}
+	public void lieAlertVolume(float sliderValue){
+		lieAlert.volume = sliderValue;
+	}
+
+	//calm
+	public void calmButton() {
+		heartRateSlider.value = Random.Range (0.2f, 0.5f);
+		respPitchSlider.value = Random.Range (0.6f, 1.6f);
+	}
+
+	public void  ExcitedTrue() {
+		heartRateSlider.value =  Random.Range (0.5f, 0.8f);
+		float random = Random.Range (0.0f, 1.0f);
+		if (random <= 0.5f) {
+			respPitchSlider.value = Random.Range (heartRateSlider.value * 3f, heartRateSlider.value * 3f + 0.01f);
+		} else {
+			respPitchSlider.value = Random.Range (heartRateSlider.value * 3f - 0.1f, heartRateSlider.value * 3f);
+		}
+		truthSlider.value = 0.4f;
+	}
+
+	public void ExcitedLie() {
+		heartRateSlider.value = Random.Range (0.1f, 0.3f);
+		respPitchSlider.value = Random.Range (2.8f, 3.0f);
+		lieSlider.value = 0.8f;
+	}
+
+	public void Moderate() {
+		heartRateSlider.value = Random.Range (0.2f, 0.4f);
+		float random = Random.Range (0.0f, 1.0f);
+		if (random <= 0.5f) {
+			respPitchSlider.value = Random.Range (1.5f, 3f);
+		} else {
+			respPitchSlider.value = Random.Range (0.11f, 1.4f);
+		}
+		GSRSlider.value = 0.6f;
 	}
 }
